e24366614746d0f41bbb7daaca1dbdcdaa5d8eed
Dr Srija Katta <drsrija@ieee.org>
2019-07-30 09:27:40 +0530

    Added traffic lights

--- blue-demo.sh
+++ blue-demo.sh
@@ -6,6 +6,16 @@ git clone https://github.com/ai4inclusion/hindawi2019
 
 pushd hindawi2019
 sudo ./install
+pushd Hindawi/samples
+hincc HindiASM.uhin
+./hin.exe
+hincc HindiC++.uhin
+./hin.exe
+popd
+popd
+
+pushd auto-vahan/Kush
+./caffe-inst-test
 popd
 
 #Add autonomous ambulance code here in Hindi
